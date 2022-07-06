aa16b3791ac174735b4caaad4687f7cd82693976
Elielton <elielton.ferreira07@outlook.com>
2022-07-06 11:44:39 -0300

    voltando para o commit 30ed4b1

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
 console.log("Rodando o sistena de gerenciamento de jogos")
-console.log("aplicação está se conectando ao bando de dados")
-// mais um console log de teste
-console.log("Enviando para o GitHub")
